9d49ae95e1ff39bfa083a74087ef4aebbec7a560
simon <simon@DESKTOP-I6LA7AG.lan>
2018-12-06 13:45:27 +0100

    Added javadoc for: 	led.Led

--- src/it/edu/majoranapa/led/Led.java
+++ src/it/edu/majoranapa/led/Led.java
@@ -1,63 +1,74 @@
 package it.edu.majoranapa.led;
 
 public class Led {
-	/*
-	*	Con questa classe, descrivo il funzionamento del singolo led (rosso, giallo o blu che sia)
-	*/
-	private int int_intensity = 0; //Intensità del led in valori interi (0-255)
-	private String hex_intensity = "00"; //Intensità del led in valori esadecimali (00-ff)
+	/**
+	 * 	The led class lets the user create a bind to a single led.
+	 * 	attributes:
+	 * 		- int_intensity: int
+	 * 		- hex_intensity: int
+	 * 	methods:
+	 * 		+ Led(intensity: int)
+	 * 		+ setIntensity(intensity: int): boolean
+	 * 		+ setIntensity(intensity: String): boolean
+	 * 		+ getIntIntensity(): int
+	 * 		+ getHexIntensity(): String
+	 */
+	private int int_intensity = 0; 
+	private String hex_intensity = "00"; 
 	
 	public Led(int intensity) 
 	{
-		/*
-		*	Questa funzione speciale, chiamata costruttore, è la funzione che viene richiamata all'inizializzazione dell'oggetto,
-		*	e serve ad inizializzare gli attributi della classe
-		*/
+		/**
+		 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
+		 * sets the led intensity based on the input parameter.
+		 */
 		if(intensity > 255 || intensity < 0)
 			return;
 		this.int_intensity = intensity;	
 		this.hex_intensity = Integer.toHexString(intensity);
 	}
 	
-	//Metodo richiamato per impostare l'intensità del led tramite un valore intero
 	public boolean setIntensity(int intensity)	
 	{
+		/**
+		 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
+		 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
+		 */
 		this.int_intensity = intensity;
 		this.hex_intensity = Integer.toHexString(intensity);
-		return false;	//Dichiaro il corretto funzionamento del metodo (funzione)
+		return false;	
 	}
 
-	//Metodo richiamato per impostare l'intensità del led tramite un valore di stringa (esadecimale)
 	public boolean setIntensity(String intensity)	
 	{
+		/**
+		 * This method requires a String parameter containing the hex intensity - 00 to ff - and
+		 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
+		 */
 		this.hex_intensity = intensity;
-		/*
-		*	Se l'utente non inserisce un valore esadecimale, la funzione genera un eccezione (crash).
-		*	Col questo costrutto intercetto l'eccezione per non far fermare il programma (try-catch).
-		*/
 		try {	
 			this.int_intensity = Integer.parseInt(intensity, 16);
 		}
 		catch(Exception exc)
 		{
-			System.out.println("Value not in hex format.");	//System.out.println serve per stampare a schermo una Stringa.
+			System.out.println("Value not in hex format.");	
 		}
-		return false;	//Dichiaro il corretto funzionamento del metodo
+		return false;	
 	}
 	
 	public int getIntIntensity()
 	{
-		/*
-		*	Restituisco il valore intero dell'attuale intensità.
-		*	Essendo l'attributo privato, cercare di accedere all'attributo usando obj.int_intensity non avrebbe alcun effetto
-		*	e genererebbe un errore di compilazione.
-		*/
+		/**
+		 * This method returns the led intensity as an int.
+		 */
 		return this.int_intensity;
 	}
 	
 	public String getHexIntensity()
 	{
-		//	Restituisco il valore esadecimale dell'attuale intensità.
+		/**
+		 * 	This method returns the led Intensity as a hex String.
+		 */
 		return this.hex_intensity;
 	}
 }
