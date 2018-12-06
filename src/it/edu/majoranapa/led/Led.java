0e53229687650f7d5a721e16cf04a9f51f1aa49f
simon <simon@DESKTOP-I6LA7AG.lan>
2018-12-06 13:53:29 +0100

    Updated javadoc for: 	led.Led

--- src/it/edu/majoranapa/led/Led.java
+++ src/it/edu/majoranapa/led/Led.java
@@ -16,35 +16,39 @@ public class Led {
 	private int int_intensity = 0; 
 	private String hex_intensity = "00"; 
 	
+	/**
+	 * @param intensity
+	 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
+	 * sets the led intensity based on the input parameter.
+	 */
 	public Led(int intensity) 
 	{
-		/**
-		 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
-		 * sets the led intensity based on the input parameter.
-		 */
 		if(intensity > 255 || intensity < 0)
 			return;
 		this.int_intensity = intensity;	
 		this.hex_intensity = Integer.toHexString(intensity);
 	}
 	
+	/**
+	 * @param intensity
+	 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
+	 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
+	 */
 	public boolean setIntensity(int intensity)	
 	{
-		/**
-		 * This method requires an int parameter containing the integer intensity - 0 to 255 - and
-		 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
-		 */
 		this.int_intensity = intensity;
 		this.hex_intensity = Integer.toHexString(intensity);
 		return false;	
 	}
 
+	/**
+	 * @param intensity
+	 * @return
+	 * This method requires a String parameter containing the hex intensity - 00 to ff - and
+	 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
+	 */
 	public boolean setIntensity(String intensity)	
 	{
-		/**
-		 * This method requires a String parameter containing the hex intensity - 00 to ff - and
-		 * sets the led intensity based on the input parameter. It returns false if everything worked correctly.
-		 */
 		this.hex_intensity = intensity;
 		try {	
 			this.int_intensity = Integer.parseInt(intensity, 16);
@@ -56,19 +60,21 @@ public class Led {
 		return false;	
 	}
 	
+	/**
+	 * @return
+	 * This method returns the led intensity as an int.
+	 */
 	public int getIntIntensity()
 	{
-		/**
-		 * This method returns the led intensity as an int.
-		 */
 		return this.int_intensity;
 	}
 	
+	/**
+	 * @return
+	 * This method returns the led Intensity as a hex String.
+	 */
 	public String getHexIntensity()
 	{
-		/**
-		 * 	This method returns the led Intensity as a hex String.
-		 */
 		return this.hex_intensity;
 	}
 }
