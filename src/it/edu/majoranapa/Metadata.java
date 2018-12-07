18eb974e1499a5fc6da6304deed7db5a8aab7a70
simon <simon@DESKTOP-I6LA7AG.lan>
2018-12-07 20:22:25 +0100

    test

--- src/it/edu/majoranapa/Metadata.java
+++ src/it/edu/majoranapa/Metadata.java
@@ -2,7 +2,7 @@ package it.edu.majoranapa;
 
 public class Metadata {
 	private final String version = "0.1";
-	private final String release_date = "05/12/2018";
+	private final String release_date = "07/12/2018";
 	
 	public String getVersion() {
 		return version;
