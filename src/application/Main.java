0a39e8adeccfb2343cfa99d84d27983490b16154
Simone Aronica <simonearonica@gmail.com>
2019-01-14 16:48:57 +0100

    Modified interface
    Added gaussian blur to background and removed window border

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import application.controller.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
@@ -22,6 +23,7 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 			root.setId("anchor");
 			Scene scene = new Scene(root, 800,480);
 			scene.getStylesheets().add(getClass().getResource("css/Main.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setResizable(false);
 			//primaryStage.setFullScreen(true);
 			primaryStage.setTitle("Home Assistant");
